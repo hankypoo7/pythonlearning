3a1bf0e1f1e949f146f3d0604f01d9fe63ea0afd
Hankypoo7 <100436822+Hankypoo7@users.noreply.github.com>
2023-04-30 17:03:24 +0000

    Pending changes exported from your codespace

---
+++ main.py
@@ -0,0 +1,52 @@
+#start setup and backround ~~~~~~~~~~~~~~~~~~~~~~
+
+
+#end setup and backround ~~~~~~~~~~~~~~~~~~~~~~~~
+#starting variables ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+name = "Student"
+
+
+#end starting variables ~~~~~~~~~~~~~~~~~~~~~~~~~
+#start game ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+print("Hello, you have been chosen to go to middle school at Indian Hills.")
+print("Goal? Survive.")
+print(" ")
+print("What is your name?")
+
+print(" ")
+print("[1] lil boi")
+print(" ")
+print("[2] Thing 1")
+print(" ")
+print("[3] cool boi)")
+print(" ")
+print("[4] Henry")
+print(" ")
+
+choice = input("")
+
+
+if choice == "1":
+    print("Got it. Lil Boi, you got this.")
+    name = "Lil Boi"
+
+if choice == "2":
+    print("Ok, Thing 1, gotime.")
+    name = "Thing 1"
+
+if choice == "3":
+    print("Cool Boi, good luck.")
+    name = "Cool Boi"
+
+if choice == "4":
+    print("The best name. Wise. I wish you the best of luck on your journey.")
+    name = "Henry"
+
+print(name + ", what team are you on?")
+
+
+
+
+#end game ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
